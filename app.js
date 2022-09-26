aa92736ce201fb4a6cc5f0c412564e77dfebf109
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-09-26 11:45:10 -0400

    updated user schema for mongoose encryption

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/usersDB", {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
     email: String,
     password: String
-};
+});
 
 const User = new mongoose.model("User", userSchema);
 
